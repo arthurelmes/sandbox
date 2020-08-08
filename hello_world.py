fa1d50c143fbf5223a8096d8a13ada5bd86c5e49
Arthur Elmes <arthur.elmes@gmail.com>
2020-08-08 15:53:39 -0400

    Updated hello_world.py with more options.

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,15 @@ Author: Arthur Elmes
 2020-08-08"""
 
 print('Hello, World!')
-a = 'Hello not Pete'
-print(a)
+a = 'Josh'
+
+#print(a)
+
 if 'Pete' in a:
     print('Josh has not been seen in years')
+elif 'Josh' in a:
+    print('Oh, hi Josh!')
+else:
+    print('Hello? Anybody?')
+
+
