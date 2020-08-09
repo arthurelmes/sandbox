1caf256ad169cd0d77d2430d2e286749a5e9719a
CottonTrow <Bilyhasemail@gmail.com>
2020-08-09 08:24:47 -0400

    test

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,10 @@
 """This is a hello world program.
 Author: Arthur Elmes
 2020-08-08"""
+#Goof Troop
+Goof_Troop = "Pete", "Josh", "Arthur", "Bily"
 
+#Wake Up
 print('Hello, World!')
 name = input('May I ask who woke me up this morning?:')
 
@@ -9,7 +12,7 @@ name = input('May I ask who woke me up this morning?:')
 #print(name)
 
 if 'Pete' in name:
-    print('Josh has not been seen in years')
+    print('Guten Morgen Pete')
 elif 'Josh' in name:
     print('Oh, hi Josh!')
 elif 'Arthur' in name:
