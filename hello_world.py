8b72018eef601c759582aa641bf6c7cb621d43a4
CottonTrow <Bilyhasemail@gmail.com>
2020-08-09 07:57:04 -0400

    test

--- hello_world.py
+++ hello_world.py
@@ -3,14 +3,19 @@ Author: Arthur Elmes
 2020-08-08"""
 
 print('Hello, World!')
-a = 'Josh'
+name = input('May I ask who woke me up this morning?:')
+
 
 #print(a)
 
-if 'Pete' in a:
+if 'Pete' in name:
     print('Josh has not been seen in years')
-elif 'Josh' in a:
+elif 'Josh' in name:
     print('Oh, hi Josh!')
+elif 'Arthur' in name:
+    print('Good morning Captain!')
+elif 'Bily' in name:
+    print('Good morning Mr. Astronaut')
 else:
     print('Hello? Anybody?')
 
